03093697b3ff2edde27be2650217341f20332f2b
Restyled.io <commits@restyled.io>
2020-10-01 11:43:08 +0000

    Restyled by astyle

--- Sorting/quick_sort/quick_sort.cpp.cpp
+++ Sorting/quick_sort/quick_sort.cpp.cpp
@@ -7,88 +7,88 @@ QUICK SORT ALGORITHM : 	TIME COMPLEXITY O(nlogn(n))
 #include<iostream>
 using namespace std;
 
-//note: call by address in swap function 
+//note: call by address in swap function
 void swap(int *a,int *b)
 {
-	int t;
-	t=*a;
-	*a=*b;
-	*b=t;
+    int t;
+    t=*a;
+    *a=*b;
+    *b=t;
 }
 
 
 //partition function which makes partition in array and return the partition index
-int partition(int a[],int low ,int high)
+int partition(int a[],int low,int high)
 {
-	// i is leftmost index and j is the rightmost index
-	int i = low,j = high;     
-	int pivot = a[low];     
-	
-	
-	do{
-		do
-		{
-		  i++;		
-		}while(a[i]<=pivot);  //note :condition is for continuation of loop ans not for stop
-		
-		do
-		{
-			j--;
-		}while(a[j]>pivot);   // note: condition is for continuation of loop and not for stop
-		
-		
-		if(i<j)              // note: condition
-			swap(&a[i],&a[j]);  // note:
-			
-	}while(i<j);              //note:condition
-	
-	swap(&a[low],&a[j]); //note:
-    
-	return j;  
+    // i is leftmost index and j is the rightmost index
+    int i = low,j = high;
+    int pivot = a[low];
+
+
+    do {
+        do
+        {
+            i++;
+        } while(a[i]<=pivot);  //note :condition is for continuation of loop ans not for stop
+
+        do
+        {
+            j--;
+        } while(a[j]>pivot);   // note: condition is for continuation of loop and not for stop
+
+
+        if(i<j)              // note: condition
+            swap(&a[i],&a[j]);  // note:
+
+    } while(i<j);              //note:condition
+
+    swap(&a[low],&a[j]); //note:
+
+    return j;
 }
 
 
 //Quick sort function
 void Quick_sort(int a[],int low,int high)
 {
-	int j; // j is partition index
-	
-	 //note: This condition for says that min two elements are required for quick sort
-	if(low < high)          
-	{
-		//index j is partition index 
-		j = partition(a,low,high) ;  
-		
-		// left partition  // note: low to j is for left partition
-		Quick_sort(a,low,j);       
-		
-		//right partition    //note: j+1 to high is for right partition
-		Quick_sort(a,j+1,high);  
-		
-	}
+    int j; // j is partition index
+
+    //note: This condition for says that min two elements are required for quick sort
+    if(low < high)
+    {
+        //index j is partition index
+        j = partition(a,low,high) ;
+
+        // left partition  // note: low to j is for left partition
+        Quick_sort(a,low,j);
+
+        //right partition    //note: j+1 to high is for right partition
+        Quick_sort(a,j+1,high);
+
+    }
 }
 
 
 //main function
-int main(){
-	
-	int arr[]= {2,4,3,6,9,8, -1};	
-	int n = sizeof(arr)/sizeof(arr[0]);
-		
-	cout<<"Before Quick Sort array is : ";
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
-	
-	//Quick sort
-	Quick_sort(arr,0,n);
-	
-	cout<<"After Quick Sort array is : ";
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
+int main() {
+
+    int arr[]= {2,4,3,6,9,8, -1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Before Quick Sort array is : ";
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    //Quick sort
+    Quick_sort(arr,0,n);
+
+    cout<<"After Quick Sort array is : ";
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 }
 
 
