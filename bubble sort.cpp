9f7810b66b1b7e3f51c7f242f5318093394cd689
Restyled.io <commits@restyled.io>
2020-10-01 08:10:25 +0000

    Restyled by clang-format

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -2,39 +2,33 @@
 
 using namespace std;
 
-int main()
-{
-    int i, j;
-    int k;
-    cout << "Please enter length: ";
-    cin >> k;
-    int numb[k];
-    for(i=0; i<=k-1; i++)
-    {
-        cout << "Please enter number: ";
-        cin >> numb[i];
+int main() {
+  int i, j;
+  int k;
+  cout << "Please enter length: ";
+  cin >> k;
+  int numb[k];
+  for (i = 0; i <= k - 1; i++) {
+    cout << "Please enter number: ";
+    cin >> numb[i];
+  }
+
+  for (i = 0; i <= k - 2; i++) {
+    for (j = i + 1; j <= k - 1; j++) {
+      int temp;
+
+      if (numb[i] > numb[j]) {
+        temp = numb[i];
+        numb[i] = numb[j];
+        numb[j] = temp;
+      }
     }
+  }
 
-    for(i=0; i<=k-2; i++)
-    {
-        for(j=i+1; j<=k-1; j++)
-        {
-            int temp;
+  for (i = 0; i <= k - 1; i++) {
+    cout << endl << numb[i] << endl;
+  }
 
-            if(numb[i] > numb[j])
-            {
-                temp = numb[i];
-                numb[i] = numb[j];
-                numb[j] = temp;
-            }
-        }
-    }
-
-    for(i=0; i<=k-1; i++)
-    {
-        cout << endl <<  numb[i] << endl;
-    }
-
-    cin.ignore();
-    cin.get();
+  cin.ignore();
+  cin.get();
 }
